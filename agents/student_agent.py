30fe8cb51e51b87079e8071e2340841066ee4375
Steven Kim <steven.shk12@gmail.com>
2023-12-05 05:40:10 -0500

    Fixed return formula in openSapceHeuristic

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -304,7 +304,7 @@ class StudentAgent(Agent):
                 for direction in range(0, 4):
                     if chessBoard[x, y, direction]:
                         totalWalls += 1
-        return (totalSquares - totalWalls) / totalSquares
+        return (totalWalls - totalWalls) / totalSquares
     
     def extendBarrierHeuristic(self, move, chessBoard):
         score = 0
